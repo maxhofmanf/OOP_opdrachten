0c846f579c4b1656fea86b5adf524685506fb7bd
maxhofmanf <99068281@mydavinci.nl>
2023-04-21 11:38:20 +0200

    OOP_opdracht 1

---
+++ opdracht2/app/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace game
+{
+    class Charmander
+    {
+        string? name;
+        string weakness = "";
+        string strength = "";
+        public Charmander(string? name, string weakness, String strength)
+        {
+            this.name = name;
+            this.weakness = weakness;
+            this.strength = strength;
+        }
+        public void battleCry()
+        {
+            Console.WriteLine(this.name + " used battle cry: " + this.name + "!");
+        }
+    }
+    class BattleSim
+    {
+        static void Main(string[] args)
+        {
+
+            string? antwoord = "";
+            while (antwoord != "quit")
+            {
+                Console.Write("hoe wil je charmander noemen? : ");
+                string? new_name;
+                new_name = Console.ReadLine();
+                Charmander charmander = new Charmander(new_name, "water", "fire");
+                for (int x = 0; x < 10; x++)
+                {
+                    charmander.battleCry();
+                }
+                Console.Write("type quit to stop: ");
+                antwoord = Console.ReadLine();
+            }
+        }
+    }
+}
