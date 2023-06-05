1a929aaad1b63c046b25ddc5a91e9d7c668fd864
maxhofmanf <99068281@mydavinci.nl>
2023-06-05 09:11:18 +0200

    opdracht 4 wip

---
+++ opdracht 4/opdracht 4/Pokemon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+abstract class Pokemon
+{
+ 
+ 
+    public string name, strength, weakness;
+    public Pokemon(string name, string strength, string weakness)
+    {
+        this.name = name;
+        this.strength = strength;
+        this.weakness = weakness;
+
+    }
+    public abstract void battleCry();        
+}
+
+class Charmander: Pokemon
+{
+   public Charmander(string name):base(name,"fire", "water")
+    {
+        this.name = name;
+    }
+    public override void battleCry()
+    {
+        Console.WriteLine(this.name + " used battle cry: Charmander!");
+    }
+}
+class Bulbasaur : Pokemon
+{
+    public Bulbasaur(string name) : base(name, "leaf", "fire")
+    {
+        this.name = name;
+    }
+    public override void battleCry()
+    {
+        Console.WriteLine(this.name + " used battle cry: Bulba!");
+    }
+}
+class Squirtle : Pokemon
+{
+    public Squirtle(string name) : base(name, "water", "leaf")
+    {
+        this.name = name;
+    }
+    public override void battleCry()
+    {
+        Console.WriteLine(this.name + " used battle cry: Squirtle!");
+    }
+}
