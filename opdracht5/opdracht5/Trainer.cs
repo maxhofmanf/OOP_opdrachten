bc9ae44bc253b7e612fd7031dc321849e4770102
maxhofmanf <99068281@mydavinci.nl>
2023-06-26 14:23:08 +0200

    finsihed product

--- opdracht5/opdracht5/Trainer.cs
+++ opdracht5/opdracht5/Trainer.cs
@@ -7,42 +7,47 @@ using System.Threading.Tasks;
 
 internal class Trainer
 {
+    private const int MaximumPokeballs = 6;
+    private const int MaximumTypePokemon = 2;
+
     public Pokemon? pokemon;
     public string? Name;
     public List<Pokeball> belt;
-    public Trainer(string? Name)
+
+    public Trainer(string? name)
     {
-        this.Name = Name;
+        Name = name;
         belt = new List<Pokeball>();
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < MaximumPokeballs; i++)
         {
             belt.Add(new Pokeball());
-            
-            if (i < 2)
+
+            if (i < MaximumTypePokemon)
             {
                 belt[i].Capture(new Charmander("sparky"));
             }
-            else if (i == 2 || i == 3 )
+            else if (i >= MaximumTypePokemon && i < MaximumTypePokemon * 2)
             {
                 belt[i].Capture(new Bulbasaur("bunger"));
-
-
             }
-            else if (i == 4 || i == 5)
+            else if (i >= MaximumTypePokemon * 2 && i < MaximumPokeballs)
             {
                 belt[i].Capture(new Squirtle("pepe"));
-
+            }
+            else if (i >= MaximumPokeballs)
+            {
+                Console.WriteLine("There are too many pokeballs on your trainer's belt.");
             }
         }
+
         Random random = new Random();
         belt = belt.OrderBy(i => random.Next()).ToList();
     }
+
     public void ThrowBall(int number, Trainer trainer)
     {
         Console.WriteLine(trainer.Name + " sends out " + belt[number].pokemon?.name);
-
     }
-
 }
 
